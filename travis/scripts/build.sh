693a1222282700a07a52070cf1a34444f8580b34
朱宋宇 <>
2019-12-09 15:27:13 +0800

    test

--- travis/scripts/build.sh
+++ travis/scripts/build.sh
@@ -3,15 +3,3 @@ xcrun xcodebuild -project TravisTest.xcodeproj -scheme TravisTest
 
 xcrun xcodebuild -exportArchive -archivePath TravisTest.xcarchive \
   -exportPath ./build -exportOptionsPlist ExportOptions.plist
-
-#!/usr/bin/env bash
-
-#if [[ $TRAVIS_BRANCH == 'master' ]];then
-#    fastlane pgy
-#elif [[ $TRAVIS_BRANCH == 'testflight' ]];then
-#    fastlane test
-#elif [[ $TRAVIS_BRANCH == 'firtest' ]];then
-#    fastlane firtest
-#else
-#    fastlane fir
-#fi
